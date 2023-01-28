0f7eb4710cf809f072d960aeaa0a1f687372a952
rifaldhiaw <rifaldhiaw@gmail.com>
2023-01-28 12:39:43 +0700

    add ScrollArea

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
 import { Label } from "@/components/ui/Label";
 import { EventPager } from "@/components/ui/Pagination";
+import { ScrollArea } from "@/components/ui/ScrollArea";
 import {
   Select,
   SelectContent,
@@ -102,6 +103,17 @@ export default function Home() {
             </SelectGroup>
           </SelectContent>
         </Select>
+
+        {/* scroll area example */}
+        <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4">
+          Jokester began sneaking into the castle in the middle of the night and
+          leaving jokes all over the place: under the king&apos;s pillow, in his
+          soup, even in the royal toilet. The king was furious, but he
+          couldn&apos;t seem to stop Jokester. And then, one day, the people of
+          the kingdom discovered that the jokes left by Jokester were so funny
+          that they couldn&apos;t help but laugh. And once they started
+          laughing, they couldn&apos;t stop.
+        </ScrollArea>
       </main>
     </>
   );
