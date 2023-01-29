89aa67269491dc70e55d2c30adff0054d5791d0c
rifaldhiaw <rifaldhiaw@gmail.com>
2023-01-29 14:46:32 +0700

    fix CI

--- e2e/products.spec.ts
+++ e2e/products.spec.ts
@@ -7,3 +7,17 @@ test("has table", async ({ page }) => {
   const table = await page.waitForSelector("table");
   expect(table).toBeTruthy();
 });
+
+test("has required table headers", async ({ page }) => {
+  await page.goto("/products");
+
+  // Expect a table with headers: Product Name, Brand, Price, Stock, Category
+  const table = await page.waitForSelector("table");
+  const headers = await table.$$("th");
+  expect(headers.length).toBe(5);
+  expect(await headers[0].innerText()).toBe("Product Name");
+  expect(await headers[1].innerText()).toBe("Brand");
+  expect(await headers[2].innerText()).toBe("Price");
+  expect(await headers[3].innerText()).toBe("Stock");
+  expect(await headers[4].innerText()).toBe("Category");
+});
